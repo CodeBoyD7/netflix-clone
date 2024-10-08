93858f7de48234dff3470b0c19cd2c8a8ad862b3
vasuD7 <vasudeva2002y@gmail.com>
2024-10-08 17:01:35 +0530

    Update the footer

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -6,6 +6,6 @@ export const ENV_VARS = {
 	MONGO_URI: process.env.MONGO_URI,
 	PORT: process.env.PORT || 5000,
 	JWT_SECRET: process.env.JWT_SECRET,
-	NODE_ENV: process.env.NODE_ENV,
+	NODE_ENV: process.env.NODE_ENV || "development",
 	TMDB_API_KEY: process.env.TMDB_API_KEY,
 };
